8ad606a8d2c1b80977f4b377dfaf0ad5fda3a3e4
MasterVVK <vvk@2140101.ru>
2025-10-30 11:35:16 +0000

    Up

--- test_pro.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-import os
-
-#genai.configure(api_key="AIzaSyDQFynSk4rV-tSYAviGjASgSDG_fU8wLgo")
-genai.configure(api_key="AIzaSyCMsmqNiBKvwzctgTisqQCGXwBH1y-IL2k")
-model = genai.GenerativeModel("gemini-2.5-pro")
-
-response = model.generate_content("Краткое описание алгоритмов кластеризации")
-print(response.text)
